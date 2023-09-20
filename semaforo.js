c5aee5b9428cbe608122fe0f08e968ba64ebd6f0
nicolefernandads <137309766+nicolefernandads@users.noreply.github.com>
2023-09-20 11:00:53 -0300

    Add files via upload

---
+++ semaforo.js
@@ -0,0 +1,26 @@
+const vermelho = document.querySelector('.vermelho');
+const amarelo = document.querySelector('.amarelo');
+const verde = document.querySelector('.verde');
+
+let contador = 1;
+
+setInterval(() =>{
+    if(contador <= 1) {
+        amarelo.classList.add('amarelo-ativo');
+    } else if (contador === 5) {
+        vermelho.classList.add('vermelho-ativo');
+        amarelo.classList.remove('amarelo-ativo');
+    }else if (contador === 10) {
+        amarelo.classList.add('amarelo-ativo');
+        vermelho.classList.remove('vermelho-ativo');
+    }else if (contador === 15) {
+        verde.classList.add('verde-ativo');
+        amarelo.classList.remove('amarelo-ativo');
+    }else if (contador === 20) {
+        remove.classList.remove('verde-ativo');
+        contador = 0;
+    }
+    
+
+    contador += 1;
+}, 1000);
